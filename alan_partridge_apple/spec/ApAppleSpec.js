a7ffb1c6bb8bbf13983f31720691c42fc4740ad1
Miranda Wilson <mirandascwilson@gmail.com>
2021-04-21 21:01:49 +0100

    Return the correct string as appropriate

--- alan_partridge_apple/spec/ApAppleSpec.js
+++ alan_partridge_apple/spec/ApAppleSpec.js
@@ -1,7 +1,12 @@
 describe("apple turnover", function () {
   const yorkie = 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  const sun = "It's hotter than the sun!!"
 
   it("returns Yorkie for 1", function () {
     expect(apple(1)).toEqual(yorkie)
+  })
+
+  it("returns Yorkie for 1", function () {
+    expect(apple(50)).toEqual(sun)
   })
 })
--- alan_partridge_apple/src/ApApple.js
+++ alan_partridge_apple/src/ApApple.js
@@ -1,3 +1,6 @@
 function apple(x) {
+  if (x**2 > 1000) {
+    return "It's hotter than the sun!!"
+  }
   return 'Help yourself to a honeycomb Yorkie for the glovebox.'
 }
