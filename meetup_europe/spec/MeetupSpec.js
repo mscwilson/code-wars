36f92b2d2e6cf697964ca3e9ed2cc0d8ee6b6577
Miranda Wilson <mirandascwilson@gmail.com>
2021-04-21 22:06:16 +0100

    Test for language on single object

--- meetup_europe/spec/MeetupSpec.js
+++ meetup_europe/spec/MeetupSpec.js
@@ -1,6 +1,13 @@
 describe('meetup', function () {
+
   it('returns 1 for a basic object with Europe and JS', function () {
     let guestList = [{continent: 'Europe', language: 'JavaScript' }]
     expect(countDevelopers(guestList)).toEqual(1)
   })
+
+  it('returns 0 for a basic object without JS', function () {
+    let guestList = [{continent: 'Europe', language: 'Ruby' }]
+    expect(countDevelopers(guestList)).toEqual(0)
+  })
+
 })
--- meetup_europe/src/Meetup.js
+++ meetup_europe/src/Meetup.js
@@ -1,3 +1,6 @@
 function countDevelopers(list) {
+  if (list[0].language !== "JavaScript") {
+    return 0
+  }
   return 1
 }
