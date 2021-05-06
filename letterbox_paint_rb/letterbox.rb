7c1a120f1e172019f1bc39e8ddb3384ff1100e9a
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 20:17:16 +0100

    Refactor to make it more intuitive

--- letterbox_paint_rb/letterbox.rb
+++ letterbox_paint_rb/letterbox.rb
@@ -1,4 +1,4 @@
 def paint_letter_boxes(start_door, end_door)
-  numbers =  (start_door..end_door).to_a.join
-  ("0".."9").map { |i| numbers.count(i) }
+  numbers = (start_door..end_door).to_a.join
+  (0..9).map { |i| numbers.count(i.to_s) }
 end
