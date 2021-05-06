79f48e09c3ee479842c2b8eb3b9954e8056cf5a6
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 11:52:16 +0100

    Get solution using each

--- number_digit_tiers_rb/array_tiers.rb
+++ number_digit_tiers_rb/array_tiers.rb
@@ -1,3 +1,12 @@
 def create_array_of_tiers(num)
-  [num.to_s]
+  digits = num.to_s.chars
+  result = []
+  digits.each do |digit|
+    if result.empty?
+      result << digit
+    else
+      result << result.last + digit
+    end
+  end
+  result
 end
--- number_digit_tiers_rb/spec/array_tiers_spec.rb
+++ number_digit_tiers_rb/spec/array_tiers_spec.rb
@@ -6,8 +6,15 @@ describe "numbers to arrays" do
     expect(create_array_of_tiers(1)).to eq ["1"]
   end
 
-  xit "works for 420" do
+  it "works for 25" do
+    expect(create_array_of_tiers(25)).to eq ["2", "25"]
+  end
+
+  it "works for 420" do
     expect(create_array_of_tiers(420)).to eq ["4", "42", "420"]
   end
 
+  it "works for 80200" do
+    expect(create_array_of_tiers(80200)).to eq ["8", "80", "802", "8020", "80200"]
+  end
 end
