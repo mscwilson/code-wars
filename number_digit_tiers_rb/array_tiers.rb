5cb0d5a9041946d1571271b2f2d72d35679337c1
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 11:53:57 +0100

    Get solution using each

--- number_digit_tiers_rb/array_tiers.rb
+++ number_digit_tiers_rb/array_tiers.rb
@@ -8,5 +8,15 @@ def create_array_of_tiers(num)
       result << result.last + digit
     end
   end
+
+  digits.map_with_index do |digit, i|
+    if i == 0
+      digit
+    else
+      digits[i - 1] + digit
+    end
+  end
+  p digits
+
   result
 end
