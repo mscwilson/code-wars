05ad2d3e6cca26daa886449ca2b46ca20a3dd2cc
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 20:03:17 +0100

    Get solution using Array.new with a block

--- letterbox_paint_rb/spec/letterbox_spec.rb
+++ letterbox_paint_rb/spec/letterbox_spec.rb
@@ -7,7 +7,10 @@ describe "letterbox painting" do
   end
 
   it "gives right answer for two spanning digits" do
-    str = "string"
     expect(paint_letter_boxes(2, 5)).to eq [0, 0, 1, 1, 1, 1, 0, 0, 0, 0]
+  end
+
+  it "gives right answer for two spanning double digits" do
+    expect(paint_letter_boxes(34, 35)).to eq [0, 0, 0, 2, 1, 1, 0, 0, 0, 0]
   end
 end
