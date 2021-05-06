995b98817f28a1ae812c939fa7d125bb2f6cedd1
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 14:38:00 +0100

    Finish numbers to array tier with map

--- number_digit_tiers_rb/array_tiers.rb
+++ number_digit_tiers_rb/array_tiers.rb
@@ -1,22 +1,4 @@
 def create_array_of_tiers(num)
   digits = num.to_s.chars
-
-  digits.map!.with_index do |digit, i|
-    # if i == 0
-    #   digit
-    # else
-      digits[i - 1] + digit
-    # end
-  end
-
-  digits.map_with_index do |digit, i|
-    if i == 0
-      digit
-    else
-      digits[i - 1] + digit
-    end
-  end
-  p digits
-
-  result
+  digits.map!.with_index { |digit, i| i == 0 ? digit : digits[i - 1] + digit }
 end
