c2fc648e1e8d9a8318cd17345caf68cb34c9cb78
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 14:32:17 +0100

    Fix a git problem

--- number_digit_tiers_rb/array_tiers.rb
+++ number_digit_tiers_rb/array_tiers.rb
@@ -2,11 +2,11 @@ def create_array_of_tiers(num)
   digits = num.to_s.chars
 
   digits.map!.with_index do |digit, i|
-    if i == 0
-      digit
-    else
+    # if i == 0
+    #   digit
+    # else
       digits[i - 1] + digit
-    end
+    # end
   end
   p digits
 
