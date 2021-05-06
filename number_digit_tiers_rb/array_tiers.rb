f3817e3bbc07822cb00dd8ec9ea1d75db132a6fa
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-06 14:57:01 +0100

    Updated solution using a range

--- number_digit_tiers_rb/array_tiers.rb
+++ number_digit_tiers_rb/array_tiers.rb
@@ -1,4 +1,4 @@
 def create_array_of_tiers(num)
-  digits = num.to_s.chars
-  digits.map!.with_index { |digit, i| i == 0 ? digit : digits[i - 1] + digit }
+  nums = num.to_s
+  (0..nums.length - 1).map { |range| nums[0..range] }
 end
