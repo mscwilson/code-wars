e0b78232c4790f654ac6677ac41b336fac851d66
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-08 14:56:59 +0100

    Finish missing values kata

--- missing_values_rb/missing_values.rb
+++ missing_values_rb/missing_values.rb
@@ -1,3 +1,9 @@
 def missing_values(numbers)
-  18
+  mapped = (0...numbers.uniq.length).map do |i|
+    [numbers.uniq[i], numbers.count(numbers.uniq[i])]
+  end
+  x = mapped.find { |pair| pair[1] == 1 }.first
+  y = mapped.find { |pair| pair[1] == 2 }.first
+
+  x * x * y
 end
--- missing_values_rb/spec/missing_values_spec.rb
+++ missing_values_rb/spec/missing_values_spec.rb
@@ -5,4 +5,9 @@ describe "missing values" do
   it "returns 18 for the basic given example" do
     expect(missing_values([1, 1, 1, 2, 2, 3])).to eq 18
   end
+
+  it "returns 80 for [1, 1, 1, 4, 5, 5]" do
+    expect(missing_values([1, 1, 1, 4, 5, 5])).to eq 80
+  end
+
 end
